97545c62edcf7b5fc23176718809f15d12f1c9fd
namtq184161 <quangnam260800@gmail.com>
2023-03-27 23:19:47 +0700

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,143 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'firstName is required'],
+      trim: true,
+      text: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, 'lastName is required'],
+      trim: true,
+      text: true,
+    },
+    username: {
+      type: String,
+      required: [true, 'username is required'],
+      trim: true,
+      text: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+    },
+    picture: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    cover: {
+      type: String,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: [true, 'gender is required'],
+      trim: true,
+    },
+    bYear: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    bMonth: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    bDay: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    requests: {
+      type: Array,
+      default: [],
+    },
+    search: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+    details: {
+      bio: {
+        type: String,
+      },
+      otherName: {
+        type: String,
+      },
+      job: {
+        type: String,
+      },
+      workplace: {
+        type: String,
+      },
+      highSchool: {
+        type: String,
+      },
+      college: {
+        type: String,
+      },
+      currentCity: {
+        type: String,
+      },
+      hometown: {
+        type: String,
+      },
+      relationship: {
+        type: String,
+        enum: ['Single', 'In a relationship', 'Married', 'Divorced'],
+      },
+      instagram: {
+        type: String,
+      },
+    },
+    savedPosts: [
+      {
+        post: {
+          type: Schema.Types.ObjectId,
+          ref: 'Post',
+        },
+        savedAt: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
