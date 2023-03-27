e21fec10444223fb1d841190b2fe0d1b248f2232
namtq184161 <quangnam260800@gmail.com>
2023-03-27 21:14:36 +0700

    feat: setup server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('welcome from home');
+});
+
+app.listen(8000, () => {
+  console.log('server is listening...');
+});
