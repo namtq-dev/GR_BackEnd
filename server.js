92caed159fa08cc7a4e87f2f0dc1daf6fcd6503c
namtq184161 <quangnam260800@gmail.com>
2023-03-27 22:46:10 +0700

    feat: use json format

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
+app.use(express.json());
 
 // Routes
 readdirSync('./routes').map((route) =>
