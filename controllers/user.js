acc399d832befeda7934ea642907caaf1a566659
namtq184161 <quangnam260800@gmail.com>
2023-03-28 21:13:55 +0700

    feat: user register + email validation

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,40 @@
-exports.home = (req, res) => {
-  res.send('welcome from user homepage');
+const { validateEmail } = require('../helpers/validation');
+const User = require('../models/user');
+
+exports.register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    } = req.body;
+
+    if (!validateEmail(email)) {
+      return res.status(400).json({
+        message: 'invalid email address',
+      });
+    }
+
+    return;
+    const user = await new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    }).save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
