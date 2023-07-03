143b5ec5179af0cd5b74b940b9b1a18dcea7565a
namtq184161 <quangnam260800@gmail.com>
2023-07-03 20:57:12 +0700

    feat: post model and create post router

---
+++ models/post.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    type: {
+      type: String,
+      enum: ['profilePicture', 'cover', null],
+      default: null,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: Array,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    background: {
+      type: String,
+    },
+    comments: [
+      {
+        comment: {
+          type: String,
+        },
+        image: {
+          type: String,
+        },
+        commentBy: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        commentAt: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', postSchema);
