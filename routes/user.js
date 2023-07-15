08a14a936356260e307ca5e74ced038554fb25ff
namtq184161 <quangnam260800@gmail.com>
2023-07-15 09:02:41 +0700

    chore: change request type

--- routes/user.js
+++ routes/user.js
@@ -27,21 +27,24 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/activate', authUser, activateAccount);
 router.post('/login', login);
+
 router.post('/sendVerification', authUser, sendVerification);
 router.post('/findUser', findUser);
 router.post('/sendResetPasswordCode', sendResetPasswordCode);
 router.post('/validateResetCode', validateResetCode);
 router.post('/changePassword', changePassword);
+
 router.get('/getProfile/:username', authUser, getProfile);
 router.patch('/updateProfilePicture', authUser, updateProfilePicture);
 router.patch('/updateCoverPicture', authUser, updateCoverPicture);
 router.patch('/updateDetails', authUser, updateDetails);
+
 router.patch('/addFriend/:id', authUser, addFriend);
 router.delete('/cancelFriendRequest/:id', authUser, cancelFriendRequest);
 router.patch('/follow/:id', authUser, follow);
 router.patch('/unfollow/:id', authUser, unfollow);
 router.patch('/acceptFriend/:id', authUser, acceptFriend);
-router.patch('/unfriend/:id', authUser, unfriend);
+router.delete('/unfriend/:id', authUser, unfriend);
 router.delete('/deleteFriendRequest/:id', authUser, deleteFriendRequest);
 
 module.exports = router;
