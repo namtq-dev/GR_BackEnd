b2d46f8ccf744ca45d0c166c0fe6697221429d7f
namtq184161 <quangnam260800@gmail.com>
2023-07-24 09:15:33 +0700

    feat: send/receive messages between 2 client

--- socket.js
+++ socket.js
@@ -10,4 +10,16 @@ exports.socketServices = (socket) => {
     socket.join(conversationId);
     // console.log(conversationId);
   });
+
+  // send and receive message
+  socket.on('send message', (message) => {
+    // console.log(message);
+    let conversation = message.conversation;
+
+    if (!conversation.users) return;
+    conversation.users.forEach((user) => {
+      if (user._id === message.sender._id) return;
+      socket.in(user._id).emit('receive message', message);
+    });
+  });
 };
